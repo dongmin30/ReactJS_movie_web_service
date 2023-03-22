9022008c6e0cbe79210a34cc4a9b00eff5e4df75
dongminkim <dongmin.kim@klimsolution.co.kr>
2023-03-22 09:15:39 +0900

    coin api를 통한 구매 가능 coin 수 계산기 commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [haveMoney, setHaveMoney] = useState(0);
   const onSubmit = (event) => {
+    const moneyInput = document.querySelector("#insertMn");
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+    setHaveMoney(moneyInput.value);
+    moneyInput.value = null;
+
   };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, [haveMoney]);
+
+  
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
+        <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+        <div>You have {haveMoney}$</div>
+        <input id="insertMn" type="number" placeholder="Insert your have money"></input>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select>
+            {coins.map((coin, index) => (
+              // haveMoney가 coin의 가격보다 큰 경우에만 옵션을 출력
+              haveMoney >= coin.quotes.USD.price && (
+                <option key={index}>
+                  {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+                </option>
+              )
+            ))}
+          </select>
+        )}
       </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
     </div>
   );
 }
