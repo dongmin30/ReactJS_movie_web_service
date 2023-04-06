6725c6ba5846a81687c9dbed8de50cae8c043fe8
Dongmin_Kim <dongmin0355@gmail.com>
2023-04-06 17:57:36 +0900

    react components 분리 및 Movies가져오는 api를 통한 데이터 화면 출력기능 추가

--- src/App.js
+++ src/App.js
@@ -1,49 +1,6 @@
-import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [haveMoney, setHaveMoney] = useState(0);
-  const onSubmit = (event) => {
-    const moneyInput = document.querySelector("#insertMn");
-    event.preventDefault();
-    setHaveMoney(moneyInput.value);
-    moneyInput.value = null;
-
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, [haveMoney]);
-
-  
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-        <div>You have {haveMoney}$</div>
-        <input id="insertMn" type="number" placeholder="Insert your have money"></input>
-        {loading ? (
-          <strong>Loading...</strong>
-        ) : (
-          <select>
-            {coins.map((coin, index) => (
-              // haveMoney가 coin의 가격보다 큰 경우에만 옵션을 출력
-              haveMoney >= coin.quotes.USD.price && (
-                <option key={index}>
-                  {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-                </option>
-              )
-            ))}
-          </select>
-        )}
-      </form>
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            // key는 React.js 에서만, map안에서 component들을 render할 때 사용한다.
+            <Movie
+              key={movie.key}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
